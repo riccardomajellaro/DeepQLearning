91239b632e072b2ee120bbdad7881fa289223df5
Dimitrios Ieronymakis <dimitris.ieronymakis@gmail.com>
2022-04-02 16:25:49 +0200

    todo + results + basic exps update

--- experiments/basic_exps.sh
+++ experiments/basic_exps.sh
@@ -112,6 +112,77 @@ python experiment.py \
 -epsilon 0.02 0.99 200. \
 -run_name MLP_tm_wait_1000 ;
 
+echo "~~~MLP + buffer + target~~"
+# MLP + buffer 128
+python experiment.py \
+-net mlp \
+-loss mse \
+-optimizer adam \
+-optim_lr 1e-3 \
+-rb_size 100000 \
+-batch_size 258 \
+-target_model \
+-tm_wait 10 \
+-n_episodes 1000 \
+-gamma 0.99 \
+-policy egreedy \
+-epsilon 0.02 0.99 200. \
+-run_name MLP_buffer_target ;
+
+echo "~~~MLP + buffer + target + softmax ~~~"
+# MLP + buffer + target
+python experiment.py \
+-net mlp \
+-loss mse \
+-optimizer adam \
+-optim_lr 1e-3 \
+-rb_size 100000 \
+-batch_size 258 \
+-target_model \
+-tm_wait 10 \
+-n_episodes 1000 \
+-gamma 0.99 \
+-policy softmax \
+-temp 0.1 \
+-run_name MLP_buffer_target_softmax ;
+
+
+echo "~~~MLP + buffer + target + custom rewards ~~~"
+# MLP + buffer + target + custom reward
+python experiment.py \
+-net mlp \
+-loss mse \
+-optimizer adam \
+-optim_lr 1e-3 \
+-rb_size 100000 \
+-batch_size 258 \
+-target_model \
+-tm_wait 10 \
+-n_episodes 1000 \
+-gamma 0.99 \
+-policy egreedy \
+-epsilon 0.02 0.99 200. \
+-custom_reward \
+-run_name MLP_buffer_target_custom-rew ;
+
+echo "~~~MLP + buffer + target + novelty ~~~"
+# MLP + buffer + target
+python experiment.py \
+-net mlp \
+-loss mse \
+-optimizer adam \
+-optim_lr 1e-3 \
+-rb_size 100000 \
+-batch_size 258 \
+-target_model \
+-tm_wait 10 \
+-n_episodes 1000 \
+-gamma 0.99 \
+-policy egreedy \
+-epsilon 0.02 0.99 200. \
+-intr_rew novelty-based \
+-run_name MLP_buffer_target_novelty ;
+
 echo "~~~CNN base~~~"
 # CNN base
 python experiment.py \
@@ -210,3 +281,68 @@ python experiment.py \
 -double_dql \
 -virtual_display \
 -run_name CNN_double ;
+
+echo "~~CNN double curiosity~~"
+# CNN curiosity
+python experiment.py \
+-use_img \
+-double_dql \
+-net cnn \
+-loss mse \
+-optimizer adam \
+-optim_lr 1e-4 \
+-rb_size 5000 \
+-batch_size 128 \
+-n_episodes 1000 \
+-gamma 0.99 \
+-target_model \
+-tm_wait 10 \
+-policy egreedy \
+-epsilon 0.025 0.99 400. \
+-intr_rew curiosity-based \
+-virtual_display \
+-run_name CNN_curiosity_double ;
+
+echo "~~CNN dueling curiosity~~"
+# CNN curiosity
+python experiment.py \
+-use_img \
+-dueling \
+-net cnn \
+-loss mse \
+-optimizer adam \
+-optim_lr 1e-4 \
+-rb_size 5000 \
+-batch_size 128 \
+-n_episodes 1000 \
+-gamma 0.99 \
+-target_model \
+-tm_wait 10 \
+-policy egreedy \
+-epsilon 0.025 0.99 400. \
+-intr_rew curiosity-based \
+-virtual_display \
+-run_name CNN_curiosity_dueling ;
+
+echo "~~CNN double dueling curiosity~~"
+# CNN curiosity
+python experiment.py \
+-use_img \
+-double_dql \
+-dueling \
+-net cnn \
+-loss mse \
+-optimizer adam \
+-optim_lr 1e-4 \
+-rb_size 5000 \
+-batch_size 128 \
+-n_episodes 1000 \
+-gamma 0.99 \
+-target_model \
+-tm_wait 10 \
+-policy egreedy \
+-epsilon 0.025 0.99 400. \
+-intr_rew curiosity-based \
+-virtual_display \
+-run_name CNN_curiosity_double_dueling ;
+
