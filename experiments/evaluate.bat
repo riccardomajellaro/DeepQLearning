21659803889e20140e3eaca99e3f9f3aa46c950e
riccardomajellaro <riccardomajellaro@gmail.com>
2022-04-04 00:55:36 +0200

    exp

--- experiments/evaluate.bat
+++
@@ -1,5 +0,0 @@
-python evaluate.py ^
--net tl_cnn ^
--use_img ^
--virtual_display ^
--run_name TL_BCE_curiosity_226
---
+++ produce_plots.py
@@ -0,0 +1,27 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.signal import savgol_filter
+
+def smooth(y, window=35, poly=1):
+    '''
+    y: vector to be smoothed 
+    window: size of the smoothing window '''
+    return savgol_filter(y,window,poly)
+
+def add_plot(title, label, y):
+    ax.set_xlabel('Episode')
+    ax.set_ylabel('Step')
+    if title is not None:
+        ax.set_title(title)
+    if label is not None:
+        ax.plot(smooth(y), label=label)
+    else:
+        ax.plot(smooth(y))
+    ax.legend()
+
+name = "CNN"
+fig, ax = plt.subplots()
+for conf in ["CNN_base", "CNN_curiosity"]:
+    steps = np.load("./exp_results/"+conf+".npy")
+    add_plot(name + " - Steps per episode", conf.replace("_", " "), steps)
+fig.savefig(name+".png", dpi=300)
