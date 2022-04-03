007c3e05164798a7151914f8fbbbdd3835bd4c7a
riccardomajellaro <riccardomajellaro@gmail.com>
2022-04-03 17:34:01 +0200

    experiments

--- experiments/basic_exps.sh
+++ experiments/basic_exps.sh
@@ -415,3 +415,42 @@ python experiment.py \
 -intr_rew curiosity-based \
 -virtual_display \
 -run_name SSL_curiosity ;
+
+# TL base
+echo "~~TL base~~"
+python experiment.py \
+-use_img \
+-net tl_cnn \
+-tl_mode 0 \
+-loss mse \
+-optimizer adam \
+-optim_lr 1e-4 \
+-rb_size 5000 \
+-batch_size 128 \
+-n_episodes 1000 \
+-gamma 0.99 \
+-target_model \
+-tm_wait 10 \
+-policy egreedy \
+-epsilon 0.025 0.99 400. \
+-run_name TL_base ;
+
+# TL curiosity
+echo "~~TL with curiosity~~"
+python experiment.py \
+-use_img \
+-net tl_cnn \
+-tl_mode 0 \
+-loss mse \
+-optimizer adam \
+-optim_lr 1e-4 \
+-rb_size 5000 \
+-batch_size 128 \
+-n_episodes 1000 \
+-gamma 0.99 \
+-target_model \
+-tm_wait 10 \
+-policy egreedy \
+-epsilon 0.025 0.99 400. \
+-intr_rew curiosity-based \
+-run_name TL_curiosity ;
